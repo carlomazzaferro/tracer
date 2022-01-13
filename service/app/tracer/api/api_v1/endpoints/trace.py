472248f78bfd0f5e350330b1011141446524a234
carlomazzaferro <carlo.mazzaferro@olx.com>
2022-01-13 10:38:49 -0300

    Crud service implementation and web3 reqs

--- service/app/tracer/api/api_v1/endpoints/trace.py
+++ service/app/tracer/api/api_v1/endpoints/trace.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from tracer.api.service import get_service
 from tracer.api.utils.db import get_db
 from tracer.schemas.block_trace import BlockTrace
 
@@ -10,7 +11,7 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[BlockTrace])
-def trace_block(
+async def trace_block(
         db: Session = Depends(get_db),
         block_number: int = 1,
         from_block: int = None,
@@ -19,3 +20,5 @@ def trace_block(
     """
     trace blocks
     """
+    service = get_service(db)
+    return await service.get_trace_for_block(block_number=block_number)
